163a007adf52f7283148b19be9ccd4c12a923224
ljyhust <lijinyang@sunlands.com>
2020-05-26 01:18:20 +0800

    [M] redisson client

--- wave-user/src/main/java/com/wave/user/config/RedissonConfig.java
+++ wave-user/src/main/java/com/wave/user/config/RedissonConfig.java
@@ -1,4 +1,12 @@
 package com.wave.user.config;
 
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RedissonConfig {
+
+    public RedissonClient getRedisClient() {
+
+    }
 }
