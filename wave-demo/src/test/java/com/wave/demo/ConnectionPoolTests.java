e7248a66f66163176ad2d5ef935db8e60a30cf64
ljyhust <lijinyang@sunlands.com>
2020-07-06 21:32:08 +0800

    [M] connection pool demo

--- wave-demo/src/test/java/com/wave/demo/ConnectionPoolTests.java
+++ wave-demo/src/test/java/com/wave/demo/ConnectionPoolTests.java
@@ -1,7 +1,11 @@
 package com.wave.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.wave.demo.concurrent.MysqlConnectionPool;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.Test;
 
 import java.sql.Connection;
@@ -9,10 +13,17 @@ import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 public class ConnectionPoolTests{
 
+    private String sql = "SELECT * FROM `user`";
+
+    private String url = "jdbc:mysql://172.16.100.102:3306/test";
+    private String userName = "eHRAdmin";
+    private String password = "shangde_wings";
     @Test
     public void testConnectionPool() throws Exception {
         List<Connection> connPool = new LinkedList<>();
@@ -29,5 +40,88 @@ public class ConnectionPoolTests{
         }
 
         Thread.sleep(60000 * 30);
+    }
+
+    @Test
+    public void testExecuteConnection() throws Exception {
+        testExecuteTest(3000, 2);
+        testExecuteTest(3000, 10);
+        testExecuteTest(3000, 20);
+        testExecuteTest(3000, 50);
+        testExecuteTest(3000, 100);
+        testExecuteTest(3000, 200);
+        testExecuteTest(3000, 300);
+        testExecuteTest(3000, 400);
+        testExecuteTest(3000, 500);
+        testExecuteTest(3000, 600);
+        testExecuteTest(3000, 700);
+    }
+
+    public ExecuteInfo testExecuteTest(int threadSize, int poolSize) throws Exception {
+        MysqlConnectionPool connectionPool = new MysqlConnectionPool(url, userName, password);
+        connectionPool.setPoolSize(poolSize);
+        connectionPool.init();
+        ExecuteInfo executeInfo = new ExecuteInfo(0, Integer.MAX_VALUE, 0);
+        Long maxTime = 0L;
+        Long minTime = (long)Integer.MAX_VALUE;
+        CountDownLatch downLatch = new CountDownLatch(threadSize);
+        for(int i = 0; i < threadSize; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    long s1 = System.currentTimeMillis();
+                    // 执行语句
+                    Connection connection = null;
+                    try {
+                        connection = connectionPool.getConnection();
+                        boolean execute = connection.prepareStatement(sql).execute();
+                        //log.info("====> sql excute res is {}", execute);
+                    } catch (Exception e) {
+                        log.info("===> {}", e);
+                    } finally {
+                        if (null != connection) {
+                            try {
+                                connectionPool.returnConnection(connection);
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    long s2 = System.currentTimeMillis();
+                    long thisTime = s2 - s1;
+                    if (thisTime > executeInfo.getMaxTime()) {
+                        executeInfo.setMaxTime(thisTime);
+                    }
+                    if (thisTime < executeInfo.getMinTime()) {
+                        executeInfo.setMinTime(thisTime);
+                    }
+                    executeInfo.setMeanTime(executeInfo.getMeanTime() + thisTime);
+                    downLatch.countDown();
+                }
+            }).start();
+        }
+        downLatch.await();
+        executeInfo.setMeanTime(executeInfo.getMeanTime() / threadSize);
+        log.info("====> poolSize {} ; executeInfo {}", poolSize, executeInfo.toString());
+        return executeInfo;
+    }
+
+    @Data
+    class ExecuteInfo {
+        private long maxTime;
+
+        private long minTime;
+
+        private long meanTime;
+
+        public ExecuteInfo(long maxTime, long minTime, long meanTime) {
+            this.maxTime = maxTime;
+            this.minTime = minTime;
+            this.meanTime = meanTime;
+        }
+
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+        }
     }
 }
